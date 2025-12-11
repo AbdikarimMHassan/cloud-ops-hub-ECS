49b664907b0770d990b5e556aeeb69bb0c0b455f
AbdikarimMHassan <abdikarimmh98@gmail.com>
2025-12-11 02:43:15 +0000

    trigger email notification when avg cpu util exceeds set threshold of 80%

---
+++ infra/modules/SNS/variables.tf
@@ -0,0 +1,21 @@
+variable "sns_topic_name" {
+    type = string
+  
+}
+
+variable "sns_subscription_alarm" {
+    type = string
+  
+}
+
+variable "sns_protocol" {
+    type = string
+  
+}
+
+variable "sns_alert_endpoint" {
+    type = string
+  
+}
+
+
